4d183b065d2dd92675fc058391d51e1144945510
shafay07 <shafay07@gmail.com>
2017-12-15 03:58:18 +0800

    Re-implement GUI
    The GUI has been re-implemented. The updated GUI fxml file has the ability to provide the controller support.

--- src/MapViewer/src/sample/Controller.java
+++ src/MapViewer/src/sample/Controller.java
@@ -1,37 +1,68 @@
-package sample;
+package MapViewer.src.sample;
+
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
-import javafx.event.Event;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.input.KeyEvent;
+
 
 public class Controller {
+
     @FXML
     private ResourceBundle resources;
-
 
     @FXML
     private URL location;
 
+    @FXML
+    private Label axePosition;
 
     @FXML
-    void exitProgram(ActionEvent event) {
+    private Label boatPosition;
+
+    @FXML
+    private Label cursorPosition;
+
+    @FXML
+    private Label information;
+
+
+    @FXML
+    void aboutInfo(ActionEvent event) {
     }
 
     @FXML
-    void info(ActionEvent event) {
+    void exit(ActionEvent event) {
     }
 
     @FXML
-    void zoomInMenu(ActionEvent event) {
+    void handleKeyAction(KeyEvent event) {
     }
 
     @FXML
-    void zoomoutMenu(Event event) {
+    void handleSetPosition(KeyEvent event) {
+    }
+
+    @FXML
+    void helpInfo(ActionEvent event) {
+    }
+
+    @FXML
+    void zoomInFromMenu(ActionEvent event) {
+    }
+
+    @FXML
+    void zoomOutFromMenu(ActionEvent event) {
     }
 
     @FXML
     void initialize() {
+        assert axePosition != null : "fx:id=\"axePosition\" was not injected: check your FXML file 'Structure.fxml'.";
+        assert boatPosition != null : "fx:id=\"boatPosition\" was not injected: check your FXML file 'Structure.fxml'.";
+        assert cursorPosition != null : "fx:id=\"cursorPosition\" was not injected: check your FXML file 'Structure.fxml'.";
+        assert information != null : "fx:id=\"information\" was not injected: check your FXML file 'Structure.fxml'.";
 
 
     }
--- src/MapViewer/src/sample/Main.java
+++ src/MapViewer/src/sample/Main.java
@@ -7,6 +7,7 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+    public static Stage primaryStage;
 
     @Override
     public void start(Stage primaryStage) throws Exception{
