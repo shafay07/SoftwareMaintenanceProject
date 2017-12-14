1d817a8edf83fa56fa928e59ded458c23a3fb6ac
shafay07 <shafay07@gmail.com>
2017-12-15 02:37:11 +0800

    App Skeleton
    The primary stage has been updated. The code has been reformatted into different packages to make it easier to understand. This update provides a working GUI app.

---
+++ src/MapViewer/src/sample/Controller.java
@@ -0,0 +1,39 @@
+package sample;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.event.Event;
+import javafx.fxml.FXML;
+
+public class Controller {
+    @FXML
+    private ResourceBundle resources;
+
+
+    @FXML
+    private URL location;
+
+
+    @FXML
+    void exitProgram(ActionEvent event) {
+    }
+
+    @FXML
+    void info(ActionEvent event) {
+    }
+
+    @FXML
+    void zoomInMenu(ActionEvent event) {
+    }
+
+    @FXML
+    void zoomoutMenu(Event event) {
+    }
+
+    @FXML
+    void initialize() {
+
+
+    }
+
+}
